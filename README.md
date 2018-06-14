cf1a934a706ef604bc1076a2d171091ed4915219
Ilya Timofeev <ilya.timofeev@monopoly.su>
2018-06-14 10:59:27 +0300

    Add deps, fix README

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
-# nexign-test-task-server-clojure
-
-FIXME
-
-## Prerequisites
-
-You will need [Leiningen][] 2.0.0 or above installed.
-
-[leiningen]: https://github.com/technomancy/leiningen
-
-## Running
-
-To start a web server for the application, run:
-
-    lein ring server
-
-## License
-
-Copyright © 2018 FIXME
+To start server
+```
+STEAM_API_KEY=XXXXX lein ring server-headless
+```
+where XXXXX - your steam api key
--- src/nexign_test_task_server_clojure/handler.clj
+++ src/nexign_test_task_server_clojure/handler.clj
@@ -1,11 +1,19 @@
 (ns nexign-test-task-server-clojure.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.json :as ring-json]
+            [ring.util.response :as rr]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(def steam-api-key (System/getenv "STEAM_API_KEY"))
+
+(println (str "Provided Steam API key: " steam-api-key))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (ring-json/wrap-json-response)
+      (wrap-defaults site-defaults)))
